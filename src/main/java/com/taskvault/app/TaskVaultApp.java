9e341f7fca3af17ecf1c8f54deed4eb377cf73c5
Rafael Blumm <109688389+rafaelblumm@users.noreply.github.com>
2026-05-19 11:40:56 -0300

    feat: Implementada aplicação inicial com Spring Boot (#1)

---
+++ src/main/java/com/taskvault/app/TaskVaultApp.java
@@ -0,0 +1,13 @@
+package com.taskvault.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskVaultApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskVaultApp.class, args);
+  }
+
+}
