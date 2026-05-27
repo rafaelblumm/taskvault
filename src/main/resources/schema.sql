d789b867a54c63af0f96387b8a122bc268832a20
Rafael Blumm <rafaelfloresblumm@gmail.com>
2026-05-26 21:38:19 -0300

    feat: Implementada criação de usuário padrão

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -42,3 +42,13 @@ CREATE TABLE IF NOT EXISTS taskvault.comment (
   CONSTRAINT comment_task_FK FOREIGN KEY (task_id) REFERENCES task (id),
   CONSTRAINT comment_user_FK FOREIGN KEY (creator_id) REFERENCES user (id)
 );
+
+-- Cria usuário inicial da aplicação
+INSERT IGNORE INTO taskvault.`user` (id, name, email, role, password)
+VALUES (
+  'sudo',
+	'Super User',
+	'',
+	'SYSADMIN',
+	'$argon2id$v=19$m=16384,t=2,p=1$mEP/6VDYws4iQb5P9UwNSPukkBlVEZz87LwJm9N4ZZA$BcNWaZ9PIEVL2k0PKtaP1XGabViAgfRr/3ir5etv/wOIaZ0s42X6m+2/W629ZqQogpSyuj9796RjaeUS3SllEjBIwEKr/lFW'
+);
