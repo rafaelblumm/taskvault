3aec6fde503a8068d39b7a4c4ff5480cbf1125a2
Maurício <oprach.mauricio@gmail.com>
2026-06-01 22:11:14 -0300

    feat: Implementação do softDelete de comentários

--- src/main/java/com/taskvault/app/controller/CommentController.java
+++ src/main/java/com/taskvault/app/controller/CommentController.java
@@ -2,15 +2,10 @@ package com.taskvault.app.controller;
 
 import java.util.List;
 
+import com.taskvault.app.error.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.taskvault.app.error.TaskNotFoundException;
 import com.taskvault.app.payload.request.CommentRequest;
@@ -55,6 +50,17 @@ public class CommentController {
             .stream()
             .map(CommentResponse::from)
             .toList();
+    }
+
+    /**
+     * Deleta comentário
+     * @param commentId ID do comentário
+     * @throws CommentNotFoundException Se o comentário não existir
+     */
+    @DeleteMapping("/{commentId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable long commentId) throws CommentNotFoundException {
+        commentService.deleteComment(commentId);
     }
 
 }
--- src/main/java/com/taskvault/app/error/UserRoleNotPermitted.java
+++ src/main/java/com/taskvault/app/error/UserRoleNotPermitted.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(
-        code = HttpStatus.UNAUTHORIZED,
+        code = HttpStatus.FORBIDDEN,
         reason = "Usuário não tem permissão"
 )
 public class UserRoleNotPermitted extends RuntimeException {}
