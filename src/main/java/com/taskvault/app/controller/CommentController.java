0ddd01ca5d461cf4fba3342c80abedb265b71a07
Maurício <oprach.mauricio@gmail.com>
2026-06-01 22:29:38 -0300

    Voltando com os imports

--- src/main/java/com/taskvault/app/controller/CommentController.java
+++ src/main/java/com/taskvault/app/controller/CommentController.java
@@ -5,7 +5,14 @@ import java.util.List;
 import com.taskvault.app.error.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.taskvault.app.error.TaskNotFoundException;
 import com.taskvault.app.payload.request.CommentRequest;
